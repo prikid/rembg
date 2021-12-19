3536d1c13d996b031db3e8b7e5293bd7d986f098
Serhii <serhii.riabcheniuk@gmail.com>
2021-12-19 17:31:12 +0200

    samll fixes

--- my_request.py
+++ my_request.py
@@ -1,16 +1,32 @@
+from pathlib import Path
+
 import requests
 
 # TODO test https
 endpoint = "http://164.90.175.236:5000/api/removebg"
 
+input_folder = Path('examples')
+output_folder = Path('output')
 
-response = requests.post(
-    endpoint,
-    files={'file': open('examples/animal-1.jpg', 'rb')},
-    headers={'X-Api-Key': 'INSERT_YOUR_API_KEY_HERE'},
-)
-if response.status_code == requests.codes.ok:
-    with open('no-bg.png', 'wb') as out:
-        out.write(response.content)
-else:
-    print("Error:", response.status_code, response.text)
+output_folder.mkdir(exist_ok=True, parents=True)
+
+for img in input_folder.glob('*.jpg'):
+    if 'out' in img.name:
+        continue
+
+    print(f'Processing image f{img}...')
+
+    try:
+        response = requests.post(
+            endpoint,
+            files={'file': img.open('rb')},
+            # headers={'X-Api-Key': 'INSERT_YOUR_API_KEY_HERE'},
+        )
+        if response.ok:
+            out_img = output_folder.joinpath(img.with_suffix('.out.png').name)
+            out_img.write_bytes(response.content)
+        else:
+            raise Exception(response.status_code, response.text)
+
+    except Exception as e:
+        print(e)
